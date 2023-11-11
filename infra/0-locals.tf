bfa22f44a1b7632b62e2c19d6b42e96f449f9d39
philipdaquin <philipdaquin20@gmail.com>
2023-11-12 05:48:49 +0800

    locals variable

--- infra/0-locals.tf
+++ infra/0-locals.tf
@@ -1,5 +1,10 @@
 
 
-locals {
-  
+locals { 
+    env     = "dev"
+    key_name = "devops"
+    region  = "us-east-2"
+    zone1   = "us-east-2a"
+    zone2   = "us-east-2b"
+    instance_type = "t2.micro"
 }
